c17a49c5348ba3fd196e112dfb544fa8d4a5b844
Willard Turner <wildturner1@gmail.com>
2023-06-21 16:03:02 -0400

    very commit

--- ServerReport.ps1
+++ ServerReport.ps1
@@ -4,3 +4,6 @@ $Name = $Env:COMPUTERNAME
 # Operating System name
 $Version = (Get-ComputerInfo | Select-Object OsVersion).osversion
 
+# Logical Disk Count
+$DiskCoount = (Get-Ciminstance CIM_LogicalDisk).count
+
