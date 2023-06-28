a71a63ac5a9ae7b0aa33515001cc0b549e68959e
Willard Turner <wildturner1@gmail.com>
2023-06-28 15:03:00 -0400

    yes I am

--- ServerReport.ps1
+++ ServerReport.ps1
@@ -7,3 +7,18 @@ $Version = (Get-ComputerInfo | Select-Object OsVersion).osversion
 # Logical Disk Count
 $DiskCount = (Get-Ciminstance CIM_LogicalDisk).count
 
+# write some bad examples of output
+Write-Host "Server Name: $Name"
+
+#good example of output
+Write-output "Computer name: $Name"
+Write-Output "Computer Version: $Version"
+
+# make our own custom object
+$obj = [PSCustomObject]@{
+   ComputerName = "$Name"
+   OSVersion = "$Version"
+   DiscCount = "$DiskCount"
+}
+
+Write-output $obj
