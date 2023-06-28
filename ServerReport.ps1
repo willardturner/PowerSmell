48417e2b2c5eea90d73d911db773a296d8fa6ddb
Willard Turner <wildturner1@gmail.com>
2023-06-28 16:07:33 -0400

    added Freespace in GB to the report

--- ServerReport.ps1
+++ ServerReport.ps1
@@ -5,7 +5,10 @@ $Name = $Env:COMPUTERNAME
 $Version = (Get-ComputerInfo | Select-Object OsVersion).osversion
 
 # Logical Disk Count
-$DiskCount = (Get-Ciminstance CIM_LogicalDisk).count
+$DiskCount = (Get-Ciminstance CIM_LogicalDisk).Count
+
+# Finding the Freespace of the drive(s)
+$Freespace = (get-ciminstance CIM_LogicalDisk).freespace
 
 # write some bad examples of output
 Write-Host "Server Name: $Name"
@@ -16,9 +19,10 @@ Write-Output "Computer Version: $Version"
 
 # make our own custom object
 $obj = [PSCustomObject]@{
-   ComputerName = "$Name"
-   OSVersion = "$Version"
-   DiscCount = "$DiskCount"
+ComputerName = $Name
+OSVersion = $Version
+DiscCount = $DiskCount
+'GB Free' = $Freespace / 1Gb
 }
 
 #writing the output of the custom object 
