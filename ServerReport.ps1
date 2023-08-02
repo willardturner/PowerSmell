d2497a21b996f6b6a2429779288b40086ce03be5
Willard Turner <wildturner1@gmail.com>
2023-08-02 16:03:19 -0400

    Set the drive letter to a defined string

--- ServerReport.ps1
+++ ServerReport.ps1
@@ -1,45 +1,45 @@
 function Get-PowerSmell {
-    
+    [CmdletBinding()]
     param (
-$DriveLetter
+        [ValidateSet("C","D","E","F","G","H")]
+        [string]$DriveLetter = 'C'
     )
-# Retrieve System Name
-$Name = $Env:COMPUTERNAME
+    # Retrieve System Name
+    $Name = $Env:COMPUTERNAME
 
-# Operating System name
-$Version = (Get-ComputerInfo | Select-Object OsVersion).osversion
+    # Operating System name
+    $Version = (Get-ComputerInfo | Select-Object OsVersion).osversion
 
-# Logical Disk Count
-$DiskCount = (Get-Ciminstance CIM_LogicalDisk).Count
+    # Logical Disk Count
+    $DiskInstance = Get-Ciminstance CIM_LogicalDisk
+    $DiskCount = $DiskInstance.Count
 
-# Finding the Freespace of the drive(s)
-$Freespace = ($DiskInstance | Where-Object {$_.DeviceID -eq $DriveLetter}).freespace
+    # Finding the Freespace of the drive(s)
+    $Freespace = ($DiskInstance | Where-Object { $_.DeviceID -eq ($DriveLetter + ":") }).freespace
 
-# Homework - Adding installed programs to the server repot list
-$Printers = (get-printer).Name
+    # Homework - Adding installed programs to the server repot list
+    $Printers = (get-printer).Name
 
-# Homework - Adding System Type to Servcer Reprot 
-$systype = (Get-Computerinfo | Select-Object CsSystemType).CsSystemType
+    # Homework - Adding System Type to Servcer Reprot 
+    $systype = (Get-Computerinfo | Select-Object CsSystemType).CsSystemType
 
-# write some bad examples of output
-Write-Host "Server Name: $Name"
+    # write some bad examples of output
+    Write-Host "Server Name: $Name"
 
-#good example of output
-Write-output "Computer name: $Name"
-Write-Output "Computer Version: $Version"
+    #good example of output
+    Write-output "Computer name: $Name"
+    Write-Output "Computer Version: $Version"
 
-# make our own custom object
-$obj = [PSCustomObject]@{
-ComputerName = $Name
-OSVersion = $Version
-DiscCount = $DiskCount
-'GB Free' = [System.Math]::Round($FreeSpace / 1Gb, 2)
-'Installed Printers' = $Printers
-OStype = $systype
+    # make our own custom object
+    $obj = [PSCustomObject]@{
+        ComputerName         = $Name
+        OSVersion            = $Version
+        DiscCount            = $DiskCount
+        'GB Free'            = [System.Math]::Round($FreeSpace / 1Gb, 2)
+        'Installed Printers' = $Printers
+        OStype               = $systype
+    }
+
+    #writing the output of the custom object 
+    Write-output $obj
 }
-
-#writing the output of the custom object 
-Write-output $obj
-}
-
-Get-PowerSmell
